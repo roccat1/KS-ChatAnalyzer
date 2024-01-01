efbce75554b692876564f419bce9e54aec13b1bb
Roc Rodríguez Arumí <roc.r2005@gmail.com>
2024-01-01 23:00:02 +0100

    format, parameters, smoothed, months, years, charts, ...

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import datetime, json, xlsxwriter, os, subprocess
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
 with open("config.json", 'r') as f: config = json.load(f)
 filename = config["defaultFilePath"]
@@ -117,43 +116,198 @@ def readJson():
 def writeJsonToXls(jsonFile):
     workbook = xlsxwriter.Workbook(config["outputDirPath"]+config["outputExcelFileName"])
 
-    row = 1
-
-
-    #counts sheet
+    ############################################ COUNTS SHEET #########################################################
     countsSheet = workbook.add_worksheet("counts")
-    countsSheet.write(0, 0, 'KS Counts')
+    countsSheet.write(0, 0, 'Counts')
 
+    rowDays = 2
+    rowMonths = 5
+    rowYears = 5
+    
+    countsThisYear=0
+    
+    #cell foramat for upper part
+    cellFormatUp = workbook.add_format()
+    cellFormatUp.set_border(1)
+    cellFormatUp.set_bg_color('#C6C6C6')
+    
+    #cell foramat for lower part
+    cellFormatDown = workbook.add_format()
+    cellFormatDown.set_border(1)
+    cellFormatDown.set_bg_color('#E1E1E1')
+    
+    countsSheet.write(1, 1, 'Date', cellFormatUp)
+    countsSheet.write(1, 2, 'Counts', cellFormatUp)
+    
+    countsSheet.write(4, 4, 'Month', cellFormatUp)
+    countsSheet.write(4, 5, 'Counts', cellFormatUp)
+    countsSheet.write(4, 6, 'Average', cellFormatUp)
+    
+    countsSheet.write(4, 8, 'Year', cellFormatUp)
+    countsSheet.write(4, 9, 'Counts', cellFormatUp)
+    countsSheet.write(4, 10, 'Average', cellFormatUp)
+    
+    counts = []
+    
+    #per day counts
     for year in jsonFile["data"]:
-        countsSheet.write(row, 1, year)
-        #months
         for month in jsonFile["data"][year]:
-            countsSheet.write(row, 2, month)
-            #days
             for day in jsonFile["data"][year][month]:
-                countsSheet.write(row, 3, str(year)+"-"+str(months.index(month))+"-"+str(day))
-                countsSheet.write(row, 4, jsonFile["data"][year][month][day][0])
-                row+=1
-
-    countsChart = workbook.add_chart({'type': 'line'})
-    countsChart.add_series({
-        'name': 'Total Counts',
-        'categories': '=counts!$D$2:$D$'+str(row),
-        'values': '=counts!$E$2:$E$'+str(row),
+                #write date
+                countsSheet.write(rowDays, 1, f"{year}-{months.index(month):02}-{day:02}", cellFormatDown)
+                #write counts day
+                countsSheet.write(rowDays, 2, jsonFile["data"][year][month][day][0], cellFormatDown)
+                counts.append(jsonFile["data"][year][month][day][0])
+                
+                rowDays+=1
+                
+            #write month
+            countsSheet.write(rowMonths, 4, f"{month}-{year}", cellFormatDown)
+            #write counts month
+            countsSheet.write(rowMonths, 5, sum([jsonFile['data'][year][month][day][0] for day in jsonFile['data'][year][month]]), cellFormatDown)
+            countsThisYear+=sum([jsonFile['data'][year][month][day][0] for day in jsonFile['data'][year][month]])
+            #write average counts month
+            countsSheet.write(rowMonths, 6, round(sum([jsonFile['data'][year][month][day][0] for day in jsonFile['data'][year][month]])/len(jsonFile['data'][year][month]), 2), cellFormatDown)
+            
+            rowMonths+=1
+            
+        #write year
+        countsSheet.write(rowYears, 8, year, cellFormatDown)
+        #write counts year
+        countsSheet.write(rowYears, 9, countsThisYear, cellFormatDown)
+        #write average counts year
+        countsSheet.write(rowYears, 10, round(countsThisYear/sum([len(jsonFile['data'][year][month]) for month in jsonFile['data'][year]]), 2), cellFormatDown)
+        
+        countsThisYear=0
+        rowYears+=1
+    
+    countsSheet.write(4, 12, 'Date', cellFormatUp)
+    countsSheet.write(4, 13, 'Smoothed Counts', cellFormatUp)
+    
+    rowDays = 5
+    countsLocation=0
+    
+    #smoothed counts
+    for year in jsonFile["data"]:
+        for month in jsonFile["data"][year]:
+            for day in jsonFile["data"][year][month]:
+                if countsLocation>=config["smoothingFactor"]:
+                    #write date
+                    countsSheet.write(rowDays, 12, f"{year}-{months.index(month):02}-{day:02}", cellFormatDown)
+                    #write smoothed counts
+                    countsSheet.write(rowDays, 13, round(sum(counts[countsLocation-config["smoothingFactor"]:countsLocation+config["smoothingFactor"]+1])/(config["smoothingFactor"]*2+1), 2), cellFormatDown)
+                    
+                    rowDays+=1
+                countsLocation+=1
+    
+    
+    countsSheet.write(1, 4, 'Total Average', cellFormatUp)
+    countsSheet.write(2, 4, round(sum(counts)/len(counts), 4), cellFormatDown)
+    
+    countsSheet.write(1, 5, 'Total Sum', cellFormatUp)
+    countsSheet.write(2, 5, sum(counts), cellFormatDown)
+    
+    countsSheet.write(1, 6, 'Total Days', cellFormatUp)
+    countsSheet.write(2, 6, len(counts), cellFormatDown)
+    
+    countsSheet.write(1, 7, 'Median', cellFormatUp)
+    countsSheet.write(2, 7, sorted(counts)[len(counts)//2], cellFormatDown)
+    
+    countsSheet.write(1, 8, 'Max', cellFormatUp)
+    countsSheet.write(2, 8, max(counts), cellFormatDown)
+    
+    countsSheet.write(1, 9, 'Standard Deviation', cellFormatUp)
+    countsSheet.write(2, 9, round((sum([(i-sum(counts)/len(counts))**2 for i in counts])/(len(counts)-1))**0.5, 4), cellFormatDown)
+    
+    countsSheet.write(1, 10, 'Variance', cellFormatUp)
+    countsSheet.write(2, 10, round((sum([(i-sum(counts)/len(counts))**2 for i in counts])/(len(counts)-1)), 4), cellFormatDown)
+    
+    countsSheet.write(1, 11, 'Coefficient of Variation', cellFormatUp)
+    countsSheet.write(2, 11, round((sum([(i-sum(counts)/len(counts))**2 for i in counts])/(len(counts)-1))**0.5/(sum(counts)/len(counts)), 4), cellFormatDown)
+    
+    countsSheet.write(1, 12, 'Skewness', cellFormatUp)
+    countsSheet.write(2, 12, round((sum([(i-sum(counts)/len(counts))**3 for i in counts])/(len(counts)-1))/(round((sum([(i-sum(counts)/len(counts))**2 for i in counts])/(len(counts)-1)), 4))**(3/2), 4), cellFormatDown)
+    
+    countsSheet.write(1, 13, 'Kurtosis', cellFormatUp)
+    countsSheet.write(2, 13, round((sum([(i-sum(counts)/len(counts))**4 for i in counts])/(len(counts)-1))/(round((sum([(i-sum(counts)/len(counts))**2 for i in counts])/(len(counts)-1)), 4))**2, 4), cellFormatDown)
+    
+    
+    countsSheet.autofit()
+    
+    #total counts
+    dayCountsChart = workbook.add_chart({'type': 'line'})
+    dayCountsChart.add_series({
+        'name': 'Day Counts',
+        'categories': '=counts!$B$3:$B$'+str(rowDays),
+        'values': '=counts!$C$3:$C$'+str(rowDays),
         'trendline': {
             'type': 'moving_average',
             'period': 23,
         }
     })
-    countsChart.set_x_axis({'date_axis': True})
-    countsChartSheet = workbook.add_chartsheet("Counts Chart")
-    countsChartSheet.set_chart(countsChart)
-    #fer fulla de grafics
+    dayCountsChart.set_x_axis({'date_axis': True})
+    dayCountsChartSheet = workbook.add_chartsheet("Day Counts Chart")
+    dayCountsChartSheet.set_chart(dayCountsChart)
+    dayCountsChart.set_title({'name': 'Counts'})
+    dayCountsChart.set_x_axis({'name': 'Date'})
+    
+    #smoothed counts
+    smoothedCountsChart = workbook.add_chart({'type': 'line'})
+    smoothedCountsChart.add_series({
+        'name': 'Smoothed Counts',
+        'categories': '=counts!$M$6:$M$'+str(rowDays),
+        'values': '=counts!$N$6:$N$'+str(rowDays),
+        'trendline': {
+            'type': 'moving_average',
+            'period': 23,
+        }
+    })
+    smoothedCountsChart.set_x_axis({'date_axis': True})
+    smoothedCountsChartSheet = workbook.add_chartsheet("Smoothed Counts Chart")
+    smoothedCountsChartSheet.set_chart(smoothedCountsChart)
+    smoothedCountsChart.set_title({'name': 'Smoothed Counts'})
+    smoothedCountsChart.set_x_axis({'name': 'Date'})
+    
+    #month averages
+    monthCountsChart = workbook.add_chart({'type': 'line'})
+    monthCountsChart.add_series({
+        'name': 'Month Averages',
+        'categories': '=counts!$E$6:$E$'+str(rowMonths),
+        'values': '=counts!$G$6:$G$'+str(rowMonths),
+        'trendline': {
+            'type': 'moving_average',
+            'period': 23,
+        }
+    })
+    monthCountsChart.set_x_axis({'date_axis': True})
+    monthCountsChartSheet = workbook.add_chartsheet("Month Averages Chart")
+    monthCountsChartSheet.set_chart(monthCountsChart)
+    monthCountsChart.set_title({'name': 'Month Average Counts'})
+    monthCountsChart.set_x_axis({'name': 'Month'})
+    
+    #year averages
+    yearCountsChart = workbook.add_chart({'type': 'line'})
+    yearCountsChart.add_series({
+        'name': 'Year Averages',
+        'categories': '=counts!$I$6:$I$'+str(rowYears),
+        'values': '=counts!$K$6:$K$'+str(rowYears),
+        'trendline': {
+            'type': 'moving_average',
+            'period': 23,
+        }
+    })
+    yearCountsChart.set_x_axis({'date_axis': True})
+    yearCountsChartSheet = workbook.add_chartsheet("Year Averages Chart")
+    yearCountsChartSheet.set_chart(yearCountsChart)
+    yearCountsChart.set_title({'name': 'Year Average counts'})
+    yearCountsChart.set_x_axis({'name': 'Year'})
 
-    #hours sheet
 
+
+
+    ############################################ HOURS SHEET #########################################################
     hoursSheet = workbook.add_worksheet("hours")
-    hoursSheet.write(0, 0, 'KS Hours')
+    hoursSheet.write(0, 0, 'Hours')
 
     row = 2
     hour=datetime.datetime(2000,1,1,hour=0, minute=0)
@@ -205,30 +359,28 @@ def writeJsonToXls(jsonFile):
     workbook.close()
 
 def runProgram():
+    log("program executed... ")
     try:
         datesRaw = readWAChatDates(filename)
-        print("25% done")
-
         jsonResult = datesToJson(datesRaw)
-        print("50% done")
-
         saveJson(jsonResult)
-        print("75% done")
-
         writeJsonToXls(jsonResult)
-        print("100% done")
 
         label_file_explorer.configure(text="Program executed")
+        messagebox.showinfo("Done!", str(os.path.basename(filename))+" has been analyzed successfully!")
         subprocess.run([os.path.join(os.getenv('WINDIR'), 'explorer.exe'), os.getcwd()+"\\output"])
-    except:
+        log("successfully")
+    except Exception as e:
+        log("with an error :(")
         label_file_explorer.configure(text="ERROR(is the format correct?/check log/terminal)")
-
+        log("ERROR: "+str(e))
 
 if __name__=='__main__':
     window = tk.Tk()
     window.title('KS Project')
     window.geometry("700x300")
     window.config(background = "turquoise2")
+    window.iconbitmap("assets/icon.ico")
 
     label_file_explorer = tk.Label(window, 
 							text = "KS-ChatAnalyzer",
@@ -262,7 +414,7 @@ if __name__=='__main__':
 
     button_exit.grid(column = 1,row = 5, columnspan=2)
 
-    dd_mmFormat = tk.BooleanVar(value=True)
+    dd_mmFormat = tk.BooleanVar(value=config["dd_mmFormat"])
     DDMM_Button = tk.Radiobutton(window, text="DD_MM_YY Format", variable=dd_mmFormat,
                                 indicatoron=False, value=True, width=19, height = 2)
     MMDD_Button = tk.Radiobutton(window, text="MM_DD_YY Format", variable=dd_mmFormat,
