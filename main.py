cd7b3398b9b623d65142f364884786445051cc0b
Roc Rodríguez Arumí <roc.r2005@gmail.com>
2024-01-02 13:30:35 +0100

    config

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import datetime, json, xlsxwriter, os, subprocess
+import datetime, json, xlsxwriter, os, subprocess, webbrowser
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
-with open("config.json", 'r') as f: config = json.load(f)
+config = {
+    "projectName": "KS-ChatAnalyzer",
+    "hourDivisions": 96,
+    "smoothingFactor": 3,
+
+    "dd_mmFormat": False,
+    "defaultFilePath": "sensible/ks-chat.txt",
+    "logPath": "log.txt",
+    "outputDirPath": "output/",
+    "outputJsonFileName": "json_output.json",
+    "outputExcelFileName": "output.xlsx"
+}
+
 filename = config["defaultFilePath"]
 
 months = ["","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -375,6 +387,81 @@ def runProgram():
         label_file_explorer.configure(text="ERROR(is the format correct?/check log/terminal)")
         log("ERROR: "+str(e))
 
+def configuration():
+    #open new window
+    configWindow = tk.Toplevel()
+    configWindow.title('Configuration')
+    configWindow.geometry("700x350")
+    configWindow.config(background = "turquoise2")
+    configWindow.iconbitmap("assets/icon.ico")
+    
+    #option project name, hour divisions, smoothing factor, ouput dir path, output excel file name
+    projectName = tk.StringVar(value=config["projectName"])
+    hourDivisions = tk.IntVar(value=config["hourDivisions"])
+    smoothingFactor = tk.IntVar(value=config["smoothingFactor"])
+    outputDirPath = tk.StringVar(value=config["outputDirPath"])
+    outputExcelFileName = tk.StringVar(value=config["outputExcelFileName"])
+    
+    #project name
+    projectNameLabel = tk.Label(configWindow, text="Project Name", width=20, height = 2, fg="white", bg="black")
+    projectNameEntry = tk.Entry(configWindow, textvariable=projectName, width=50, font=("Arial", 15), border=5)
+    projectNameLabel.grid(column = 1, row = 1, sticky="w")
+    projectNameEntry.grid(column = 2, row = 1, sticky="w")
+    
+    #hour divisions
+    hourDivisionsLabel = tk.Label(configWindow, text="Hour Divisions", width=20, height = 2, fg="white", bg="black")
+    hourDivisionsEntry = tk.Entry(configWindow, textvariable=hourDivisions, width=50, font=("Arial", 15), border=5)
+    hourDivisionsLabel.grid(column = 1, row = 2, sticky="w")
+    hourDivisionsEntry.grid(column = 2, row = 2, sticky="w")
+    
+    #smoothing factor
+    smoothingFactorLabel = tk.Label(configWindow, text="Smoothing Factor", width=20, height = 2, fg="white", bg="black")
+    smoothingFactorEntry = tk.Entry(configWindow, textvariable=smoothingFactor, width=50, font=("Arial", 15), border=5)
+    smoothingFactorLabel.grid(column = 1, row = 3, sticky="w")
+    smoothingFactorEntry.grid(column = 2, row = 3, sticky="w")
+    
+    #output dir path
+    outputDirPathLabel = tk.Label(configWindow, text="Output Dir Path", width=20, height = 2, fg="white", bg="black")
+    outputDirPathEntry = tk.Entry(configWindow, textvariable=outputDirPath, width=50, font=("Arial", 15), border=5)
+    outputDirPathLabel.grid(column = 1, row = 4, sticky="w")
+    outputDirPathEntry.grid(column = 2, row = 4, sticky="w")
+    
+    #output excel file name
+    outputExcelFileNameLabel = tk.Label(configWindow, text="Output Excel File Name", width=20, height = 2, fg="white", bg="black")
+    outputExcelFileNameEntry = tk.Entry(configWindow, textvariable=outputExcelFileName, width=50, font=("Arial", 15), border=5)
+    outputExcelFileNameLabel.grid(column = 1, row = 5, sticky="w")
+    outputExcelFileNameEntry.grid(column = 2, row = 5, sticky="w")
+    
+    #save button
+    def saveConfig():
+        config["projectName"]=projectName.get()
+        config["hourDivisions"]=hourDivisions.get()
+        config["smoothingFactor"]=smoothingFactor.get()
+        config["outputDirPath"]=outputDirPath.get()
+        config["outputExcelFileName"]=outputExcelFileName.get()
+        configWindow.destroy()
+        
+    saveButton = tk.Button(configWindow, text="Save", command=saveConfig, width=20, height = 2)
+    saveButton.grid(column = 1, row = 6, columnspan=2)
+    
+    #cancel button
+    def cancelConfig():
+        configWindow.destroy()
+    
+    cancelButton = tk.Button(configWindow, text="Cancel", command=cancelConfig, width=20, height = 2)
+    cancelButton.grid(column = 1, row = 7, columnspan=2)
+    
+    #wiki button
+    def wiki():
+        webbrowser.open('https://github.com/roccat1/KS-ChatAnalyzer/wiki')
+    
+    wikiButton = tk.Button(configWindow, text="Wiki", command=wiki, width=20, height = 2)
+    wikiButton.grid(column = 1, row = 8, columnspan=2)
+    
+    #let the window wait for any events
+    configWindow.mainloop()
+    
+
 if __name__=='__main__':
     window = tk.Tk()
     window.title('KS Project')
@@ -399,6 +486,11 @@ if __name__=='__main__':
     button_run = tk.Button(window, 
 						text = "Run program",
 						command = runProgram,
+                        width = 40, height = 2)
+    
+    button_config = tk.Button(window, 
+						text = "Configuration",
+						command = configuration,
                         width = 40, height = 2) 
     
     button_exit = tk.Button(window, 
@@ -411,8 +503,10 @@ if __name__=='__main__':
     button_explore.grid(column = 1, row = 2, columnspan=2)
 
     button_run.grid(column = 1, row = 4, columnspan=2)
+    
+    button_config.grid(column = 1, row = 5, columnspan=2)
 
-    button_exit.grid(column = 1,row = 5, columnspan=2)
+    button_exit.grid(column = 1,row = 6, columnspan=2)
 
     dd_mmFormat = tk.BooleanVar(value=config["dd_mmFormat"])
     DDMM_Button = tk.Radiobutton(window, text="DD_MM_YY Format", variable=dd_mmFormat,
